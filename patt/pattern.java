40f69fa6d400b25e60c93c5e1d821c177bf64f37
ninadgandhul <ninadgandhul2@gmail.com>
2025-07-09 14:09:34 +0000

    new

---
+++ patt/pattern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=5;i++){
+            for(int y=1;y<=n-i+1;y++){
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+        
+        //System.out.println("Try programiz.pro");
+    }
+}
